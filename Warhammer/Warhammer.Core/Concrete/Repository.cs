db9c81910b7ec658953fe61f03c1dfdfcb8783ae
Tim Kaine <t.kaine@bliss-systems.co.uk>
2017-06-03 18:39:14 +0100

    Minor tidy up of repo

--- Warhammer/Warhammer.Core/Concrete/Repository.cs
+++ Warhammer/Warhammer.Core/Concrete/Repository.cs
@@ -285,6 +285,44 @@ namespace Warhammer.Core.Concrete
             BulkInsert<T>(connection, tableName, list);
         }
 
+        private void BulkInsert<T>(string connection, string tableName, IList<T> list)
+        {
+            using (var bulkCopy = new SqlBulkCopy(connection))
+            {
+                bulkCopy.BatchSize = list.Count;
+                bulkCopy.DestinationTableName = tableName;
+
+                var table = new DataTable();
+                var props = TypeDescriptor.GetProperties(typeof(T))
+
+                                           // Dirty hack to make sure we only have system data types 
+                                           // i.e. filter out the relationships/collections
+                                           .Cast<PropertyDescriptor>()
+                                           .Where(propertyInfo => propertyInfo.PropertyType.Namespace.Equals("System"))
+                                           .ToArray();
+
+                foreach (var propertyInfo in props)
+                {
+                    bulkCopy.ColumnMappings.Add(propertyInfo.Name, propertyInfo.Name);
+                    table.Columns.Add(propertyInfo.Name, Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType);
+                }
+
+                var values = new object[props.Length];
+                foreach (var item in list)
+                {
+                    for (var i = 0; i < values.Length; i++)
+                    {
+                        values[i] = props[i].GetValue(item);
+                    }
+
+                    table.Rows.Add(values);
+                }
+
+                bulkCopy.WriteToServer(table);
+            }
+        }
+        #endregion
+
         public IQueryable<FateAspect> FateAspects()
         {
             return _entities.FateAspects.Where(e => e.CampaignId == CurrentCampaignId);
@@ -597,43 +635,7 @@ namespace Warhammer.Core.Concrete
             return award.Id;
         }
 
-        private void BulkInsert<T>(string connection, string tableName, IList<T> list)
-        {
-            using (var bulkCopy = new SqlBulkCopy(connection))
-            {
-                bulkCopy.BatchSize = list.Count;
-                bulkCopy.DestinationTableName = tableName;
 
-                var table = new DataTable();
-                var props = TypeDescriptor.GetProperties(typeof(T))
-
-                                           // Dirty hack to make sure we only have system data types 
-                                           // i.e. filter out the relationships/collections
-                                           .Cast<PropertyDescriptor>()
-                                           .Where(propertyInfo => propertyInfo.PropertyType.Namespace.Equals("System"))
-                                           .ToArray();
-
-                foreach (var propertyInfo in props)
-                {
-                    bulkCopy.ColumnMappings.Add(propertyInfo.Name, propertyInfo.Name);
-                    table.Columns.Add(propertyInfo.Name, Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType);
-                }
-
-                var values = new object[props.Length];
-                foreach (var item in list)
-                {
-                    for (var i = 0; i < values.Length; i++)
-                    {
-                        values[i] = props[i].GetValue(item);
-                    }
-
-                    table.Rows.Add(values);
-                }
-
-                bulkCopy.WriteToServer(table);
-            }
-        }
-        #endregion
         public void Dispose()
         {
             Dispose(true);
