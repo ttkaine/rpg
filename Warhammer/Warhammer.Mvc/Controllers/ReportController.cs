4a467d1db5de2d4bb765f88e9a7393c574b0a6f1
Tim Kaine <tim.kaine@bliss-systems.co.uk>
2017-07-19 12:24:45 +0100

    report controller

---
+++ Warhammer/Warhammer.Mvc/Controllers/ReportController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Warhammer.Mvc.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
