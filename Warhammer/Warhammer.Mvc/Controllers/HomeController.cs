f425d8aea9e26b8cba2fffe5d05988f4e3ec135c
Tim Kaine <tim.kaine@bliss-systems.co.uk>
2017-08-16 14:38:03 +0100

    search results views (basic)

--- Warhammer/Warhammer.Mvc/Controllers/HomeController.cs
+++ Warhammer/Warhammer.Mvc/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ using Page = Warhammer.Core.Entities.Page;
 
 namespace Warhammer.Mvc.Controllers
 {
+
     public class HomeController : BaseController
     {
         private readonly IScoreCalculator _scoreCalculator;
