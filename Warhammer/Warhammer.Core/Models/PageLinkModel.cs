2f3bee8f0285b9dd65dcc2c9b5b6b57e660c9e50
Chris <chris@sendingofeight.co.uk>
2017-12-13 22:57:24 +0000

    Grouped related links into categories

--- Warhammer/Warhammer.Core/Models/PageLinkModel.cs
+++ Warhammer/Warhammer.Core/Models/PageLinkModel.cs
@@ -5,5 +5,11 @@
         public int Id { get; set; }
         public string ShortName { get; set; }
         public string FullName { get; set; }
+        public PageLinkType Type { get; set; }
+
+        public PageLinkModel()
+        {
+            
+        }
     }
 }
