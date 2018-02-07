02378b7159de03367fad066be0648f357eb0f4fd
Tim Kaine <github@timkaine.co.uk>
2018-02-07 19:05:01 +0000

    Wear edge and harm should be Low => High

--- Warhammer/Warhammer.Mvc/Concrete/ViewModelFactory.cs
+++ Warhammer/Warhammer.Mvc/Concrete/ViewModelFactory.cs
@@ -541,9 +541,9 @@ namespace Warhammer.Mvc.Concrete
                 Roles = roles,
                 Stats = stats,
                 Descriptors = npc.PersonAttributes.Where(a => a.AttributeType == AttributeType.Descriptor).Select(s => s.Name).Distinct().ToList(),
-                Wear = npc.PersonAttributes.Where(a=> a.AttributeType == AttributeType.Wear).OrderByDescending(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList(),
-                Harm = npc.PersonAttributes.Where(a => a.AttributeType == AttributeType.Harm).OrderByDescending(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList(),
-                Edge = npc.PersonAttributes.Where(a => a.AttributeType == AttributeType.Edge).OrderByDescending(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList()
+                Wear = npc.PersonAttributes.Where(a=> a.AttributeType == AttributeType.Wear).OrderBy(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList(),
+                Harm = npc.PersonAttributes.Where(a => a.AttributeType == AttributeType.Harm).OrderBy(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList(),
+                Edge = npc.PersonAttributes.Where(a => a.AttributeType == AttributeType.Edge).OrderBy(a => a.CurrentValue).Select(a => a.CurrentValue.ToString()).ToList()
             };
             return model;
         }
