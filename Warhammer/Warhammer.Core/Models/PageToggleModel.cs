83f8f8415863eca3ae1ebb3d22a7f8cadfa98622
Unknown <GitHub@timkaine.co.uk>
2018-10-23 18:49:30 +0100

    make image optional on the page toggle

--- Warhammer/Warhammer.Core/Models/PageToggleModel.cs
+++ Warhammer/Warhammer.Core/Models/PageToggleModel.cs
@@ -2,9 +2,15 @@
 {
     public class PageToggleModel
     {
+        public PageToggleModel()
+        {
+            ShowImage = true;
+        }
+
         public int PageId { get; set; }
         public string ShortName { get; set; }
         public string FullName { get; set; }
         public bool Selected { get; set; }
+        public bool ShowImage { get; set; }
     }
 }
