2faa8515ab255349ae50e439cb37500428e299ce
Tim Kaine <tim.kaine@bliss-systems.co.uk>
2019-07-15 09:56:25 +0100

    fix id to campaign id as they are not the same

--- Warhammer/Warhammer.Core/Concrete/AuthenticatedDataProvider.cs
+++ Warhammer/Warhammer.Core/Concrete/AuthenticatedDataProvider.cs
@@ -3755,8 +3755,8 @@ namespace Warhammer.Core.Concrete
 
         private List<CharacterLeagueItemModel> GetLeagueItems(IQueryable<Person> query)
         {
-            Dictionary<int, string> campaignUrls = _repository.AllCampaigns().ToDictionary(t => t.Id, t => t.Url);
-            Dictionary<int, string> campaignNames = _repository.AllCampaigns().ToDictionary(t => t.Id, t => t.DisplayName);
+            Dictionary<int, string> campaignUrls = _repository.AllCampaigns().ToDictionary(t => t.CampaignId, t => t.Url);
+            Dictionary<int, string> campaignNames = _repository.AllCampaigns().ToDictionary(t => t.CampaignId, t => t.DisplayName);
 
             int[] campaignIds = campaignUrls.Keys.ToArray();
             List <CharacterLeagueItemModel> data = query
